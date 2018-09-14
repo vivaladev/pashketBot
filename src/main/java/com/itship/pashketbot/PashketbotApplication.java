237d39f07191687c88729f16a65355319c8abed1
Egor Lebedev <lebedevev2@sovcombank.ru>
2018-09-14 15:58:38 +0400

    first bot architecture, and this isn't worked. so rnk go to hell

--- src/main/java/com/itship/pashketbot/PashketbotApplication.java
+++ src/main/java/com/itship/pashketbot/PashketbotApplication.java
@@ -1,12 +1,50 @@
 package com.itship.pashketbot;
 
+import com.itship.pashketbot.botProcessing.PashketBot;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContext;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class PashketbotApplication {
 
+    private static String PROXY_HOST = "..." /* proxy host */;
+    private static Integer PROXY_PORT = 3128 /* proxy port */;
+    private static String PROXY_USER = "..." /* proxy user */;
+    private static String PROXY_PASSWORD = "..." /* proxy password */;
+
 	public static void main(String[] args) {
-		SpringApplication.run(PashketbotApplication.class, args);
+
+
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new PashketBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+        //Add this line to initialize bots context
+        SpringApplication.run(PashketbotApplication.class, args);
+
 	}
+
+	private void setProxyVsFkinRKN(){
+
+    }
 }
---
+++ src/main/java/com/itship/pashketbot/botProcessing/PashketBot.java
@@ -0,0 +1,41 @@
+package com.itship.pashketbot.botProcessing;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class PashketBot extends TelegramLongPollingBot {
+
+    @Value("${bot_name}")
+    private String BOT_NAME;
+
+    @Value("${bot_token}")
+    private String BOT_TOKEN;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+}
